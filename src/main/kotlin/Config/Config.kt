fedeb8c72c413851c1737f78b87646176e37e8ca
shennoter <shennoter@qq.com>
2022-05-26 17:26:54 +0800

    增加额外apikey，应对api过热，一些小优化

--- src/main/kotlin/Config/Config.kt
+++ src/main/kotlin/Config/Config.kt
@@ -6,9 +6,9 @@ import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
 
 object Config : AutoSavePluginConfig("config") {
-    @ValueDescription("ApiKey：如果没有请到 https://apexlegendsapi.com/ 获取")
-    val ApiKey: String by value()
-    @ValueDescription("额外ApiKey")
+    @ValueDescription("apiKey：如果没有请到 https://apexlegendsapi.com/ 获取")
+    val apiKey: String by value()
+    @ValueDescription("额外apiKey：不同apiKey用英文逗号隔开,apiKey用英文单引号包裹，可不填")
     val extendApiKey: ArrayList<String> by value()
     @ValueDescription("回复模式: pic为图片，text为文字(适用于玩家信息、地图轮换和猎杀门槛)")
     var mode: String by value("pic")
